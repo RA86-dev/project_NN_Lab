78bdf59764208fbc5d877480c5c6f912ecbe2d37
Ra86-dev <rw98651@gmail.com>
2026-08-08 20:30:43 -0500

    swapped for CDN

--- Project Neural Network Lab/src/mnist.jsx
+++ Project Neural Network Lab/src/mnist.jsx
@@ -3,7 +3,7 @@ import * as tf from "@tensorflow/tfjs";
 export async function loadMNIST(dataset_size) {
     console.log("Downloading MNIST");
     const response = await fetch(
-        "https://raw.githubusercontent.com/RA86-dev/project_NN_Lab/refs/heads/main/Project%20Neural%20Network%20Lab/public/mnist_handwritten_test.json"
+        "https://cdn.hackclub.com/019fe424-002c-79b6-8c15-562e9eeea65a/mnist_handwritten_test.json"
     );
     const dataset = await response.json();
     console.log(
